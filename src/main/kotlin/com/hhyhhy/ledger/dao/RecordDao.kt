9f2c3a93b6ef11853f509cb417383755d504584c
hhy <huanghaoyuanhhy@gmail.com>
2022-08-18 00:35:37 +0800

    dao: remove

--- src/main/kotlin/com/hhyhhy/ledger/dao/RecordDao.kt
+++
@@ -1,8 +0,0 @@
-package com.hhyhhy.ledger.dao
-
-import com.hhyhhy.ledger.model.Record
-import com.hhyhhy.ledger.model.Records
-import org.springframework.stereotype.Component
-
-@Component
-class RecordDao : BaseDao<Record, Records>(Records)
